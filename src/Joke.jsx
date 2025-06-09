bf8fa7aac86d77d248d4549fa1e83c3a79306f42
Arvindking <arvindkuamaur8241@gmail.com>
2025-06-09 22:19:53 +0530

    Get rendom Joke

---
+++ src/Joke.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import "./Joke.css";
+
+export default function Joke() {
+  const [joke, setJoke] = useState({});
+  const url = "https://official-joke-api.appspot.com/random_joke";
+
+  const getNewJoke = async () => {
+    let response = await fetch(url);
+    let jsonResponse = await response.json();
+    console.log(jsonResponse);
+    setJoke({ setup: jsonResponse.setup, punchline: jsonResponse.punchline });
+  };
+  useEffect(() => {
+    async function getNewJoke() {
+      let response = await fetch(url);
+      let jsonResponse = await response.json();
+      setJoke({ setup: jsonResponse.setup, punchline: jsonResponse.punchline });
+    }
+    getNewJoke();
+  }, []);
+  return (
+    <>
+      <div className="joke-container">
+        <h3>JOKE</h3>
+        <h2 className="joke-setup">{joke.setup}</h2>
+        <h2 className="joke-punchline">{joke.punchline}</h2>
+        <button onClick={getNewJoke} className="joke-button">Get Joke</button>
+      </div>
+    </>
+  );
+}
